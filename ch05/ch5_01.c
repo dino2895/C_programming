5dd7c04e253d4b0480785e4f8ca10ffe7d1af800
dino2895 <dino2895un@gmail.com>
2020-10-06 10:06:04 +0800

    ch5_1.c

---
+++ ch05/ch5_01.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+int main(){
+	int m;
+	scanf("%d", &m);
+	switch(m) { 
+        case 1: 
+			printf("January");
+            break;
+		case 2: 
+			printf("February");
+            break; 
+		case 3: 
+			printf("March");
+			break; 
+		case 4: 
+			printf("April");
+			break; 
+		case 5: 
+			printf("May");
+            break; 
+		case 6: 
+			printf("June");
+            break; 
+		case 7: 
+			printf("July");
+            break; 
+		case 8: 
+			printf("August");
+            break; 
+		case 9: 
+			printf("September");
+            break; 
+		case 10: 
+			printf("October");
+            break; 
+		case 11: 
+			printf("November");
+            break; 
+		case 12: 
+			printf("December");
+            break; 
+        default: 
+            printf("error"); 
+            break;
+    } 
+}
+//January
+//February
+//March
+//April
+//May
+//June
+// July
+// August
+// September
+// October
+// November
+// December
+// error
