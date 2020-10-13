4ffd9d50d6a3239bd5b360d187623c1ced32101a
dino2895 <dino2895un@gmail.com>
2020-10-13 12:08:37 +0800

    ch06_03.c

---
+++ ch06/ch06_03.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int n=1, sum=0;
+	printf("Enter integers (0 to terminate): ");
+	while(n != 0){
+		scanf("%d", &n);
+		sum = sum+n;
+	}	
+	printf("The sum is: %d\n",sum);
+}
+	
