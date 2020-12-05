47d56b8dc28a9197bf7df09821e985f34aea9278
dino2895 <dino2895un@gmail.com>
2020-12-05 16:19:03 +0800

    prime2.c

---
+++ ch09/prime2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+bool is_prime(int n){
+	int divisor;
+	
+	if(n<=1){
+		return false;
+	}
+	for (divisor = 2 ;divisor <n;divisor++){
+		if (n%divisor==0){
+			return false;
+		}
+	}
+	return true;
+}
+int main(void){
+	int n;
+	printf ("Enter a number: ");
+	scanf("%d", &n);
+	if (is_prime(n))
+		printf("Prime\n");
+	else
+		printf("Not Prime\n");
+}
